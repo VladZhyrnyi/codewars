e48fff23ccc95ec6b7dca1bba9f6196c7b4ce5b5
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-11-06 21:11:20 +0200

    Update Word_a10n.js
    add easier solution

--- 6kyu/Word_a10n.js
+++ 6kyu/Word_a10n.js
@@ -1,36 +1,51 @@
-function abbreviate(string) {
-  let word = ''
-  const sentence = []
+// function abbreviate(string) {
+//   let word = ''
+//   const sentence = []
 
-  for (i = 0; i < string.length; i++) {
-    const char = string[i]
-    if (char.toLowerCase() != char.toUpperCase()) {
-      word += char
-    } else {
-      sentence.push(word)
-      sentence.push(char)
-      word = ''
-    }
-  }
-  if (word.length !== 0) {
-    sentence.push(word)
-  }
+//   for (i = 0; i < string.length; i++) {
+//     const char = string[i]
+//     if (char.toLowerCase() != char.toUpperCase()) {
+//       word += char
+//     } else {
+//       sentence.push(word)
+//       sentence.push(char)
+//       word = ''
+//     }
+//   }
+//   if (word.length !== 0) {
+//     sentence.push(word)
+//   }
 
-  console.log(sentence.join(''))
+//   const result = sentence
+//     .map((str) => {
+//       if (str.length < 4) {
+//         return str
+//       } else {
+//         return [str[0], str.slice(1, str.length - 1).length, str[str.length - 1]].join(
+//           ''
+//         )
+//       }
+//     })
+//     .join('')
 
-  const result = sentence
-    .map((str) => {
-      if (str.length < 4) {
-        return str
-      } else {
-        return [str[0], str.slice(1, str.length - 1).length, str[str.length - 1]].join(
-          ''
-        )
-      }
-    })
-    .join('')
+//   return result
+// }
 
-  return result
+// =============  Easier way  ===============
+
+const find = /[a-z]{4,}/gi
+
+function replace(match) {
+  return match[0] + (match.length - 2) + match[match.length - 1]
 }
 
-abbreviate('qwewq-wqeq dsrg dsfsd sdf')
+function abbreviate(string) {
+  return string.replace(find, replace)
+}
+
+console.log(
+  abbreviate(
+    'Lorem ipsum dolor sit amet-consectetur adipisicing elit. Quos, earum.'
+  )
+)
+
